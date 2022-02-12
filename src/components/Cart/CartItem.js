cc3c42afce27ab8823e3558282a152f16bf80c14
Pratik <pratikmndh@gmail.com>
2022-02-11 23:46:06 -0500

    style: add mui icons for update qty buttons

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import classes from './CartItem.module.css';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
 
 const CartItem = (props) => {
-  const price = `$${props.price.toFixed(2)}`;
-
-  return (
-    <li className={classes['cart-item']}>
-      <div>
-        <h2>{props.name}</h2>
-        <div className={classes.summary}>
-          <span className={classes.price}>{price}</span>
-          <span className={classes.quantity}>x {props.quantity}</span>
-        </div>
-      </div>
-      <div className={classes.actions}>
-        <button onClick={props.onRemove}>−</button>
-        <button onClick={props.onAdd}>+</button>
-      </div>
-    </li>
-  );
+	const price = `$${props.price.toFixed(2)}`;
+  
+	return (
+		<li className={classes['cart-item']}>
+			<div>
+				<h2>{props.name}</h2>
+				<div className={classes.summary}>
+					<span className={classes.price}>{price}</span>
+					<span className={classes.quantity}>x {props.quantity}</span>
+				</div>
+			</div>
+			<Stack sx={{ justifyContent: 'flex-end' }} direction='row' spacing={2}>
+				<Button
+					onClick={props.onRemove}
+					sx={{
+						padding: '0',
+						borderRadius: '0.5rem',
+						minWidth: '3rem',
+						height: '2.2rem',
+					}}
+					variant='outlined'>
+					<RemoveRoundedIcon />
+				</Button>
+				<Button
+					onClick={props.onAdd}
+					sx={{
+						padding: '0',
+						borderRadius: '0.5rem',
+						minWidth: '3rem',
+						height: '2.2rem',
+					}}
+					variant='outlined'>
+					<AddRoundedIcon />
+				</Button>
+			</Stack>
+		</li>
+	);
 };
 
 export default CartItem;
